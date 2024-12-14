9a91e22fe7fcee4960cb1ac5f1bc1cd6d185692e
Kuldeep Prajapati <kuldeeppraj2002@gmail.com>
2024-12-14 13:47:39 +0530

    origin

--- pro.js
+++ pro.js
@@ -13,3 +13,28 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+
+// Theme toggler functionality
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+
+// Check for saved theme in localStorage
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    body.classList.add(savedTheme);
+    themeToggle.innerHTML = savedTheme === 'dark-mode' ? '<i class="fas fa-moon"></i>' : '<i class="fas fa-sun"></i>';
+}
+
+// Toggle theme on button click
+themeToggle.addEventListener('click', () => {
+    if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
+        localStorage.setItem('theme', '');
+        themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+    } else {
+        body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark-mode');
+        themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+    }
+});
